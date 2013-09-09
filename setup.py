dfffcd46967ad2bf5ed3074165eddcec2834b065
David Snopek <dsnopek@gmail.com>
2013-09-09 17:20:02 +0100

    Added GitHub URL.

--- setup.py
+++ setup.py
@@ -3,10 +3,11 @@ from setuptools import setup, find_packages
 
 setup(
     name="AnkiServer",
-    version="2.0.0a1",
+    version="2.0.0dev",
     description="A personal Anki sync server (so you can sync against your own server rather than AnkiWeb)",
     author="David Snopek",
     author_email="dsnopek@gmail.com",
+    url="https://github.com/dsnopek/anki-sync-server",
     install_requires=["PasteDeploy>=1.3.2"],
     # TODO: should these really be in install_requires?
     requires=["webob(>=0.9.7)"],
