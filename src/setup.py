3be70a1c92e95f438cc1bebcbd63827f510a7a75
Vikash Kothary <kothary.vikash@gmail.com>
2022-07-02 14:40:12 +0100

    bump: version from 2.3.0 to 2.4.0

--- src/setup.py
+++ src/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 # TODO: Generate from or parse values from pyproject.toml.
 setup(
     name="anki-sync-server",
-    version="2.3.0",
+    version="2.4.0",
     description="Self-hosted Anki Sync Server.",
     author="Anki Community",
     author_email="kothary.vikash+ankicommunity@gmail.com",
