f6a7c2142018d2215a9d9a174baba4f6118e07ee
Vikash Kothary <kothary.vikash@gmail.com>
2022-10-14 20:55:08 +0100

    refactor: Move imports to the top of the file

--- src/ankisyncd/__main__.py
+++ src/ankisyncd/__main__.py
@@ -1,38 +1,38 @@
+import os
 import sys
 import logging
 
+from wsgiref.simple_server import make_server, WSGIRequestHandler
+
+import ankisyncd
+import ankisyncd.config
 from ankisyncd.sync_app import SyncApp
+from ankisyncd.thread import shutdown
 
 logger = logging.getLogger("ankisyncd")
 
 if __package__ is None and not hasattr(sys, "frozen"):
-    import os.path
-
     path = os.path.realpath(os.path.abspath(__file__))
     sys.path.insert(0, os.path.dirname(os.path.dirname(path)))
+
+
+class RequestHandler(WSGIRequestHandler):
+    logger = logging.getLogger("ankisyncd.http")
+
+    def log_error(self, format, *args):
+        self.logger.error("%s %s", self.address_string(), format % args)
+
+    def log_message(self, format, *args):
+        self.logger.info("%s %s", self.address_string(), format % args)
 
 
 def main():
     logging.basicConfig(
         level=logging.INFO, format="[%(asctime)s]:%(levelname)s:%(name)s:%(message)s"
     )
-    import ankisyncd
-
     logger.info(
         "ankisyncd {} ({})".format(ankisyncd._get_version(), ankisyncd._homepage)
     )
-    from wsgiref.simple_server import make_server, WSGIRequestHandler
-    from ankisyncd.thread import shutdown
-    import ankisyncd.config
-
-    class RequestHandler(WSGIRequestHandler):
-        logger = logging.getLogger("ankisyncd.http")
-
-        def log_error(self, format, *args):
-            self.logger.error("%s %s", self.address_string(), format % args)
-
-        def log_message(self, format, *args):
-            self.logger.info("%s %s", self.address_string(), format % args)
 
     if len(sys.argv) > 1:
         # backwards compat
